536197637e17be7f2def3aab5fd2309f4f1e5f91
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2022-04-15 19:28:53 +0400

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,206 @@
+/*#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#define empty 0
+#define N 9
+
+bool isGridSafe(int grid[N][N], int row, int col, int num);
+bool isEmptyLocation(int grid[N][N], int& row, int& col);
+
+bool solveSudoku(int grid[N][N])
+{
+	int row, col;
+	if (!isEmptyLocation(grid, row, col))
+		return true;
+	for (int num = 1; num <= 9; num++) {
+		if (isGridSafe(grid, row, col, num)) {
+			grid[row][col] = num;
+			if (solveSudoku(grid)) return true;
+			grid[row][col] = empty;
+		}
+	}
+	return false;
+}
+
+
+bool isEmptyLocation(int grid[N][N], int& row, int& col)
+{
+	for (row = 0; row < N; row++)
+		for (col = 0; col < N; col++)
+			if (grid[row][col] == empty)
+				return true;
+	return false;
+}
+
+
+bool usedInCol(int grid[N][N], int col1, int number)
+{
+	for (int row = 0; row < N; row++)
+		if (grid[row][col1] == number)
+			return true;
+	return false;
+}
+
+bool usedInRow(int grid[N][N], int row1, int number)
+{
+	for (int col = 0; col < N; col++)
+		if (grid[row1][col] == number)
+			return true;
+	return false;
+}
+
+
+bool usedInBox(int grid[N][N], int boxBeginRow, int boxBeginCol, int number)
+{
+	bool state = false;
+	for (int row = 0; row < 3; row++)
+		for (int col = 0; col < 3; col++)
+			if (grid[row + boxBeginRow][col + boxBeginCol] == number)
+				state = true;
+	return state;
+}
+
+
+bool isGridSafe(int grid[N][N], int row1, int col1, int number)
+{
+	return !usedInRow(grid, row1, number) &&
+		!usedInCol(grid, col1, number) &&
+		!usedInBox(grid, row1 - row1 % 3, col1 - col1 % 3, number);
+}
+
+
+void print(int grid[N][N])
+{
+	for (int row = 0; row < N; row++) {
+		for (int col = 0; col < N; col++)
+			std::cout << grid[row][col] << " ";
+		std::cout << std::endl;
+	}
+}
+
+
+int main()
+{
+	int grid[N][N] = {
+					  {0, 0, 0, 0, 0, 0, 0, 0, 0},
+					  {0, 0, 0, 0, 0, 3, 0, 8, 5},
+					  {0, 0, 1, 0, 2, 0, 0, 0, 0},
+					  {0, 0, 0, 5, 0, 7, 0, 0, 0},
+					  {0, 0, 4, 0, 0, 0, 1, 0, 0},
+					  {0, 9, 0, 0, 0, 0, 0, 0, 0},
+					  {5, 0, 0, 0, 0, 0, 0, 7, 3},
+					  {0, 0, 2, 0, 1, 0, 0, 0, 0},
+					  {0, 0, 0, 0, 4, 0, 0, 0, 9}
+	};
+
+	if (solveSudoku(grid) == true) {
+		print(grid);
+	}
+	else {
+		std::cout << "Something went wrong!";
+	}
+	return 0;
+}*/
+
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#define empty 0
+#define N 9
+
+bool isGridSafe(int grid[N][N], int row, int col, int num);
+bool isEmptyLocation(int grid[N][N], int& row, int& col);
+
+bool SolveSudoku(int grid[N][N])
+{
+	int row, col;
+	if (!isEmptyLocation(grid, row, col))
+		return true;
+	for (int num = 1; num <= 9; num++)
+	{
+		if (isGridSafe(grid, row, col, num))
+		{
+			grid[row][col] = num;
+			if (SolveSudoku(grid))
+				return true;
+			grid[row][col] = empty;
+		}
+	}
+	return false;
+}
+
+bool isEmptyLocation(int grid[N][N], int& row, int& col)
+{
+	for (row = 0; row < N; row++)
+		for (col = 0; col < N; col++)
+			if (grid[row][col] == empty)
+				return true;
+	return false;
+}
+
+bool UsedInRow(int grid[N][N], int rrow, int number)
+{
+	for (int col = 0; col < N; col++)
+		if (grid[rrow][col] == number)
+			return true;
+	return false;
+}
+
+bool UsedInCol(int grid[N][N], int coll, int number)
+{
+	for (int row = 0; row < N; row++)
+		if (grid[row][coll] == number)
+			return true;
+		else
+			return false;
+}
+
+bool UsedInBox(int grid[N][N], int boxBeginRow, int boxBeginCol, int number)
+{
+	bool state = false;
+	for (int row = 0; row < 3; row++)
+		for (int col = 0; col < 3; col++)
+			if (grid[row + boxBeginRow][col + boxBeginCol] == number)
+				state = true;
+	return state;
+}
+
+bool isGridSafe(int grid[N][N], int prow, int pcol, int number)
+{
+	return !UsedInRow(grid, prow, number) && !UsedInCol(grid, pcol, number) &&
+		!UsedInBox(grid, prow - prow % 3, pcol - pcol % 3, number);
+}
+
+void printResult(int finalgrid[N][N])
+{
+	for (int row = 0; row < N; row++)
+	{
+		for (int col = 0; col < N; col++)
+			std::cout << finalgrid[row][col] << "  ";
+		std::cout << std::endl;
+	}
+}
+
+int main()
+{
+	int grid[N][N] = {
+					  {0, 0, 0, 0, 0, 0, 0, 0, 0},
+					  {0, 0, 0, 0, 0, 3, 0, 8, 5},
+					  {0, 0, 1, 0, 2, 0, 0, 0, 0},
+					  {0, 0, 0, 5, 0, 7, 0, 0, 0},
+					  {0, 0, 4, 0, 0, 0, 1, 0, 0},
+					  {0, 9, 0, 0, 0, 0, 0, 0, 0},
+					  {5, 0, 0, 0, 0, 0, 0, 7, 3},
+					  {0, 0, 2, 0, 1, 0, 0, 0, 0},
+					  {0, 0, 0, 0, 4, 0, 0, 0, 9}
+	};
+
+	if (SolveSudoku(grid) == true)
+		printResult(grid);
+	else
+		std::cout << "Something went wrong!" << std::endl;
+	return 0;
+}
